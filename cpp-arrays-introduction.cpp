56bf7506cd81da986f5fb78964b7583fc3d7f064
H.Nathan <nathan.hervouet@ynov.com>
2018-02-13 11:48:33 +0100

    mardi

---
+++ cpp-arrays-introduction.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N;
+    int i = 0;
+    cin>>N;
+    int *A = new int[N];
+    while(cin>>A[i++]);
+    while(cout<<A[--N]<<' ' and N);
+    delete[] A;
+    return 0;
+}
